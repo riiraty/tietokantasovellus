843360fa30b8a0cacf8dbb1aaee88ea615cb5f2d
Riikka Räty <48516441+riiraty@users.noreply.github.com>
2020-04-08 13:21:46 +0300

    add feature for editing own posts

--- application/posts/forms.py
+++ application/posts/forms.py
@@ -2,7 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField, validators
 
 class PostForm(FlaskForm):
-    content = TextAreaField("Post content", [validators.Length(min=3, max=280)])
+  content = TextAreaField("Post content", [validators.Length(min=3, max=280)])
 
-    class Meta:
-        csrf = False
+  class Meta:
+    csrf = False
+
+class EditForm(FlaskForm):
+  content = TextAreaField("Edit post content", [validators.Length(min=3, max=280)])
+  
+  class Meta:
+    csrf = False
--- application/posts/views.py
+++ application/posts/views.py
@@ -4,30 +4,63 @@ from flask_login import login_required, current_user
 from application import app, db
 from application.posts.models import Post
 from application.auth.models import User
-from application.posts.forms import PostForm
+from application.posts.forms import PostForm, EditForm
 
+# listausnäkymä
 @app.route("/posts/", methods=["GET"])
 def posts_index():
-    return render_template("posts/list.html", posts = Post.query.all())
+  return render_template("posts/list.html", posts = Post.query.all(), user = current_user)
 
+# lomake uudelle postaukselle
 @app.route("/posts/new/")
 @login_required
 def posts_form():
-    return render_template("posts/new.html", form = PostForm())
+  return render_template("posts/new.html", form = PostForm())
 
+# uuden tallennus
 @app.route("/posts/", methods=["POST"])
 @login_required
 def posts_create():
-    # posted = Post(request.form.get("content"))
-    form = PostForm(request.form)
+  # posted = Post(request.form.get("content"))
+  form = PostForm(request.form)
 
-    if not form.validate():
-      return render_template("posts/new.html", form = form)
+  if not form.validate():
+    return render_template("posts/new.html", form = form)
 
-    posted = Post(form.content.data)
-    posted.account_id = current_user.id
+  posted = Post(form.content.data)
+  posted.account_id = current_user.id
 
-    db.session().add(posted)
-    db.session().commit()
+  db.session().add(posted)
+  db.session().commit()
   
+  return redirect(url_for("posts_index"))
+
+# vanhan (oman) postauksen muokkauslomake
+@app.route("/posts/<post_id>", methods=["GET"])
+@login_required
+def edit_form(post_id):
+  post = Post.query.get(post_id)
+
+  if post.account_id == current_user.id:
+    return render_template("posts/edit_post.html", form = EditForm(), post = post)
+  else:
     return redirect(url_for("posts_index"))
+
+# muokkauksen tallennus
+@app.route("/posts/<post_id>", methods=["POST"])
+@login_required
+def posts_edit(post_id):
+  post = Post.query.get(post_id)
+
+  if post.account_id == current_user.id:
+    form = EditForm(request.form)
+    newContent = form.content.data
+
+    post = Post.query.get(post_id)
+    post.content = newContent
+    db.session().commit()
+
+    return redirect(url_for("posts_index"))
+  else:
+    return redirect(url_for("posts_index"))
+
