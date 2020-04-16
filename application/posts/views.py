99dc0ed3e5b49d5f8cb99737650cf059359135d6
Riikka Räty <48516441+riiraty@users.noreply.github.com>
2020-04-16 11:37:56 +0300

    fix redirects after edit/delete

--- application/posts/views.py
+++ application/posts/views.py
@@ -12,22 +12,14 @@ from application.posts.forms import PostForm, EditForm
 # listausnäkymä
 @app.route("/posts/", methods=["GET"])
 def posts_index():
+  # haetaan ketjujen tuoreimmat postaukset (25) ja listataan
   sub = db.session.query(func.max(Post.id)).group_by(Post.thread_id).subquery()
-  posts = db.session.query(Post).filter(Post.id.in_(sub)).order_by(Post.post_time.desc()).all()
+  posts = db.session.query(Post).filter(Post.id.in_(sub)).order_by(Post.post_time.desc()).limit(25).all()
+  
   return render_template("posts/list.html",
     posts = posts,
     user = current_user
   )
-  # # postgresql group_by ei toimi, distinct valitsee ekan rivin, ei vikaa
-  #   return render_template("posts/list.html",
-  #   posts = Post.query.distinct(Post.thread_id).order_by(Post.thread_id, Post.post_time.desc()).all(),
-  #   user = current_user
-  # )
-  # sqlite 25 viimeksi kommentoitua lankaa
-  # return render_template("posts/list.html",
-  #   posts = Post.query.group_by(Post.thread_id).order_by(desc(Post.post_time)).limit(25).all(),
-  #   user = current_user
-  # )
 
 # lomake uudelle kommentille
 @app.route("/posts/<thread_id>/new/")
@@ -94,9 +86,10 @@ def posts_edit(thread_id, post_id):
     db.session().commit()
 
     flash("Your post was edited succesfully!")
-    return redirect(url_for("posts_index"))
   else:
-    return redirect(url_for("posts_index"))
+    flash("You are not authorized")
+    
+  return redirect(url_for("posts_thread", thread_id=thread_id))
 
 # oman postauksen poistaminen
 @app.route("/posts/<thread_id>/delete/<post_id>", methods=["GET", "POST"])
@@ -111,5 +104,5 @@ def posts_delete(thread_id, post_id):
   else:
     flash("You are not authorized")
 
-  return redirect(url_for("posts_index"))
+  return redirect(url_for("posts_thread", thread_id=thread_id))
 
