6f2d7d3ac120b9ca0de1bfd75ecbad31f48ba330
Riikka Räty <48516441+riiraty@users.noreply.github.com>
2020-04-30 16:24:50 +0300

    debug heroku

--- application/__init__.py
+++ application/__init__.py
@@ -6,11 +6,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 import os
 
-# if os.environ.get("HEROKU"):
-#   app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-# else:
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
-app.config["SQLALCHEMY_ECHO"] = True
+if os.environ.get("HEROKU"):
+  app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+  app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+  app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
